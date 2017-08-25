93833de7edb11d160823649d48a1f0cc8a7e1db9
Daniel <dregojevic@gmail.com>
2017-08-25 14:52:03 +0200

    initial commit with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { BuyersComponent } from './components/buyers/buyers.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    BuyersComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
